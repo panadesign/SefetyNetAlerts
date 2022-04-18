606769b50c17cfe8d8ccaee3a9f3f19596bd5a45
Jeremy <elbarto_860@hotmail.com>
2022-04-18 09:43:27 +0200

    create little method

--- src/main/java/com/openclassrooms/safetynet/DataStorage.java
+++ src/main/java/com/openclassrooms/safetynet/DataStorage.java
@@ -2,6 +2,7 @@ package com.openclassrooms.safetynet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.safetynet.model.Data;
+import com.openclassrooms.safetynet.model.FireStation;
 import com.openclassrooms.safetynet.model.Person;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -15,29 +16,48 @@ import java.util.stream.Collectors;
 @Component
 public class DataStorage {
 	private Data data;
-
+	
 	public DataStorage() throws IOException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		File file = new ClassPathResource("data.json").getFile();
 		this.data = objectMapper.readValue(file, Data.class);
 	}
-
+	
 	public Data getData() {
 		return data;
 	}
-
-	//Récupérer toutes les personnes par adresse
+	
 	public List<Person> getPersonsByAddress(String address) {
 		List<Person> allPersons = getData().getPersons();
-		List<Person> getPersonsByAddress = new ArrayList<>();
-
-		allPersons
+		
+		return allPersons
 				.stream()
 				.filter(person -> person.getAddress().equals(address))
 				.map(person -> new Person())
 				.collect(Collectors.toList());
-
-		getPersonsByAddress.addAll(allPersons);
-		return getPersonsByAddress;
+	}
+	
+	public List<Person> getPersonsByStation(Integer station) {
+		List<Person> allPersons = getData().getPersons();
+		List<FireStation> fireStations = getData().getFirestations();
+		List<Person> getPersonsByStationNumber = new ArrayList<>();
+		
+		List<FireStation> getFirestationByStationNumber =
+				fireStations
+						.stream()
+						.filter(fireStation -> fireStation.getStation() == station)
+						.map(fireStation -> new FireStation())
+						.collect(Collectors.toList());
+		
+		for (FireStation fireStation : getFirestationByStationNumber) {
+			List<Person> aggregate = allPersons
+					.stream()
+					.filter(person -> person.getAddress().equals(fireStation.getAddress()))
+					.map(person -> new Person())
+					.collect(Collectors.toList());
+			
+			getPersonsByStationNumber.addAll(aggregate);
+		}
+		return getPersonsByStationNumber;
 	}
 }
